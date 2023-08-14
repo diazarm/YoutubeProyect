e508bd548eba3fdb557b63736cdbb6545a09b3ac
diazarm <marceloardiaz@gmail.com>
2023-08-14 11:38:47 -0400

    sumado

--- prueba.js
+++ prueba.js
@@ -1,26 +1,11 @@
-// obtener el mayor de 18 anios
 
-function fizzBuzz(numero) {
-    // Si "numero" es divisible entre 3, devuelve "fizz"
-    // Si "numero" es divisible entre 5, devuelve "buzz"
-    // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
-    // De lo contrario, devuelve el numero
-    if(numero % 3 === 0 && numero % 5 === 0 ) {console.log("fizzBuzz") }
-    else if(numero % 3 === 0 ) {console.log("fizz") }
-    else if (numero % 5 ===0) {console.log("buzz") }
+let array = ['tito','pepe', 'pepito', 'tito', 'tota','tito'];
 
-    else{ console.log(numero) }
-
+function busqueda (elemento){
+   let busqueArray = []
+   for (let i=0; i<array.length; i++) {
+     if (array[i].includes(elemento)){busqueArray.push(elemento)}
+   }
+  return `la cant de veces que se repite ${elemento} es ${busqueArray.length}`
 }
-
-let array = [1,2,3,4,5,6,7]
-
-function increment (array){
-    let sumado = []
-    for (let i = 0; i<array.length; i++){
-        sumado.push(array[i]+1)
-        }
-        console.log(sumado)
-}
-
-increment(array)
+busqueda('tito')
